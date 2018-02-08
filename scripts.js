3c8b1e61102b01892e47471615d350536dcdfbbc
hachichaud <saucisse.jaune@gmail.com>
2018-02-08 16:52:57 +0100

    add lodash chain example

--- scripts.js
+++ scripts.js
@@ -34,3 +34,23 @@ $(function () {
 d3.json('flavors_of_cacao.json', function(err, data) {
   $('#cacao-data-1').text(JSON.stringify(data, null, 2));
 })
+d3.json('data-to-transform.json', function(err, data) {
+  $('#transformed-data-1').text(JSON.stringify(data, null, 2));
+
+  var curatedData = _(data.data.inTheaters)
+  .map('movies')
+  .flatten()
+  .map(function(movie){
+    return {
+      votes: movie.votes,
+      title: movie.title,
+      metascore: +movie.metascore,
+      filmingLocationsCount: movie.filmingLocations.length
+    };
+  })
+  .sortBy(function(movie) {return - movie.metascore})
+  .value()
+
+  $('#transformed-data-2').text(JSON.stringify(curatedData, null, 2))
+
+})
